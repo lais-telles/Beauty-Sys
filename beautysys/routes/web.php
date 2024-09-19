cd011b24dc51b3c5862781302eba34aa47ea4b51
Rodrigo Feitosa <114292779+Imdubitat@users.noreply.github.com>
2024-09-19 14:56:17 -0300

    Update web.php

--- beautysys/routes/web.php
+++ beautysys/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/beautysys', function () {
+    return view('index');
+});
+
+Route::get('/pessoa-física', [IndexController::class, 'PessoaFisica'])->name('PessoaFisica');
+Route::get('/parceiros', [IndexController::class, 'Parceiro'])->name('Parceiro');
+Route::get('/home-pf', [IndexController::class, 'HomePf'])->name('PaginaInicialPf');
+Route::get('/home-pj', [IndexController::class, 'HomePj'])->name('PaginaInicialPj');
+Route::get('/home-profissional', [IndexController::class, 'HomeProfissional'])->name('PaginaInicialProfissional');
+Route::get('/dashboard-pj', [IndexController::class, 'DashboardProprietario'])->name('DashboardPj');
+Route::get('/AdmPj', [IndexController::class, 'AdmProprietario'])->name('AdmProprietario');
