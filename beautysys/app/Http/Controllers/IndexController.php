d81c8a80d3c6a596cd19a82adb9a753a99bac167
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-09-27 15:52:04 -0300

    Criação de função para retornar a view 'index'

--- beautysys/app/Http/Controllers/IndexController.php
+++ beautysys/app/Http/Controllers/IndexController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    public function Index() {
+        return view('index');
+    }
+
     public function PessoaFisica() {
         return view('pessoa-fisica');
     }
