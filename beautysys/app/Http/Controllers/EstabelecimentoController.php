b8032d856937a8cfd22cef423f21047b6d19edab
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-10-10 15:47:53 -0300

    Melhoria na definição de responsabilidades entre Model e Controller

--- beautysys/app/Http/Controllers/EstabelecimentoController.php
+++ beautysys/app/Http/Controllers/EstabelecimentoController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Estabelecimento;  // Importe o modelo Estabelecimento
+use App\Models\Servico;  // Importe o modelo Servico
 use Illuminate\Support\Facades\Hash;  // Importe a classe Hash para criptografar a senha
 use Illuminate\Support\Facades\DB; // Para consultas ao banco de dados
 use Illuminate\Support\Facades\Session; // Para armazenar sessão
 
 class EstabelecimentoController extends Controller
 {
-    // Função para salvar o cliente no banco de dados
+    // Função para salvar o estabelecimento no banco de dados
     public function cadastrarEstabelecimento(Request $request)
     {
-        // Valida os dados enviados pelo modal
+        // Valida os dados enviados pelo modal presente na view
         $validatedData = $request->validate([
             'razao_social' => 'required|string|max:40',
             'nome_fantasia' => 'required|string|max:40',
@@ -31,26 +32,11 @@ class EstabelecimentoController extends Controller
             'senha' => 'required|string|min:8',
         ]);
 
-        // Cria o cliente com os dados validados e criptografa a senha
-        Estabelecimento::create([
-            'razao_social' => $validatedData['razao_social'],
-            'nome_fantasia' => $validatedData['nome_fantasia'],
-            'telefone' => $validatedData['telefone'],
-            'CNPJ' => $validatedData['cnpj'],
-            'logradouro' => $validatedData['logradouro'],
-            'numero' => $validatedData['numero'],
-            'bairro' => $validatedData['bairro'],
-            'cidade' => $validatedData['cidade'],
-            'estado' => $validatedData['estado'],
-            'CEP' => $validatedData['cep'],
-            'inicio_expediente' => $validatedData['inicio_expediente'],
-            'termino_expediente' => $validatedData['termino_expediente'],
-            'email' => $validatedData['email'],
-            'senha' => Hash::make($validatedData['senha']),
-        ]);
+        // Chama o método para criar o estabelecimento no model
+        Estabelecimento::cadastrarEstabelecimento($validatedData);
 
         // Redireciona para a página index com uma mensagem de sucesso
-        return redirect()->route('Parceiro')->with('success', 'Estabelecimento cadastrado com sucesso!');
+        return redirect()->back()->with('success', 'Estabelecimento cadastrado com sucesso!');
     }
 
     // Método de login
@@ -123,5 +109,18 @@ class EstabelecimentoController extends Controller
 
         return redirect()->back()->with('success', 'Usuário atualizado com sucesso!');
     }
+
+
+    public function listaServicos() {
+        // Captura o id do estabelecimento da sessão
+        $id_estabelecimento = Session::get('id_estabelecimento');
+   
+        // Obtém o registro do estabelecimento
+        $servicos = Servico::where('id_estabelecimento', $id_estabelecimento)->get();
+   
+        // Retorna a view com o registro
+        return view('servicos-cad', compact('servicos'));
+    }   
+
 }
 ?>
