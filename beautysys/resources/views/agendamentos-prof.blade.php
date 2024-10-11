7bfeca065583601c02f726b55184edfe76ac7d7f
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-10-11 17:23:57 -0300

    Adição de formulário para alterações no agendamento

--- beautysys/resources/views/agendamentos-prof.blade.php
+++ beautysys/resources/views/agendamentos-prof.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
 <section class="d-flex flex-column" style="margin:10rem">
+    
     <div class="mt-5">
         <h2>Agendamentos</h2>
 
@@ -50,6 +51,9 @@
                                 <button class="btn btn-primary" id="button-{{ $agendamento->id_agendamento }}" onclick="toggleSelect({{ $agendamento->id_agendamento }})">
                                     Alterar Status
                                 </button>
+                                <button class="btn btn-primary" id="button-edit-{{ $agendamento->id_agendamento }}" data-bs-toggle="modal" data-bs-target="#EditarAgendamento">
+                                    Editar
+                                </button>
                                 <form action="{{ route('agendamentosStatus') }}" method="POST" id="form-status-{{ $agendamento->id_agendamento }}" style="display:none; margin-top: 10px;">
                                     @csrf
                                     <input type="hidden" name="id_agendamento" value="{{ $agendamento->id_agendamento }}">
@@ -89,3 +93,50 @@
     }
 </script>
 @endsection
+
+<div class="modal fade" id="editarAgendamento" tabindex="-1" aria-labelledby="editarAgendamento" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content rounded-4 shadow">
+                <div class="modal-header p-5 pb-4 border-bottom-0">
+                    <h1 class="fw-bold mb-0 fs-2">Log In Estabelecimento</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body p-5 pt-0">
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <select class="form-select rounded-3" id="floatingServico" name="servicos" required>
+                                <option value="" selected disabled>Selecione o Serviço</option>
+                                @foreach ($servicos as $servico)
+                                    <option value="{{ $servico->id_servico }}">{{ $servico->nome }} - R$ {{ number_format($servico->valor, 2, ',', '.') }} ({{ $servico->duracao }})</option>
+                                @endforeach
+                            </select>
+                            <label for="floatingServico">Serviço</label>
+                        </div>
+                        
+                        <div class="form-floating mb-3">
+                            <input type="date" class="form-control rounded-3" id="floatingData" name="data">
+                            <label for="floatingData">Data</label>
+                        </div>
+                        
+                        <div class="form-floating mb-3">
+                            <input type="time" class="form-control rounded-3" id="floatingInicio" name="hora_inicio">
+                            <label for="floatingInicio">Horário início</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                        <select class="form-select rounded-3" id="floatingPag" name="pag">
+                            <option value="" selected disabled>Selecione a Forma de Pagamento</option>
+                                <option value="1">Pix</option>
+                                <option value="2">Cartão de crédito</option>
+                                <option value="3">Cartão de débito</option>
+                        </select>
+                            <label for="floatingPag">Pagamento</label>
+                        </div>
+                        
+                        <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Atualizar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
