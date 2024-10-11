985ae6ee8234c833bf74ae05aee50d4f81f2233b
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-10-11 13:23:00 -0300

    Atualização cadastral

--- beautysys/app/Models/Estabelecimento.php
+++ beautysys/app/Models/Estabelecimento.php
@@ -65,4 +65,12 @@ class Estabelecimento extends Model
         ]);
     }
     
+    public static function atualizar_estabelecimento($id_estabelecimento, $nome_fantasia, $telefone, $logradouro, $numero, $bairro, $cidade, $estado, $CEP, $inicio_expediente, $termino_expediente, $email, $senha) {
+        $estabelecimento = self::find($id_estabelecimento);
+        // Se a senha for null, mantém o valor original
+        if ($senha === null) {
+            $senha = $estabelecimento->senha; // Mantém a senha inalterada
+        }
+        return DB::statement('CALL atualizar_estabelecimento(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',[$id_estabelecimento, $nome_fantasia, $telefone, $logradouro, $numero, $bairro, $cidade, $estado, $CEP, $inicio_expediente, $termino_expediente, $email, $senha]);
+    }
 }
