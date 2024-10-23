90ba537c2763f85cf3b779759c8a80d1cd97f87c
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-10-23 13:58:55 -0300

    Ajustes de responsividade da tabela de agendamentos dos clientes

--- beautysys/resources/views/agendamentos-cliente.blade.php
+++ beautysys/resources/views/agendamentos-cliente.blade.php
@@ -3,21 +3,22 @@
 @section('title','Agendamentos')
 
 @section('content')
-<section class="d-flex flex-column" style="margin:10rem">
-    <div class="mt-5">
+<section class="d-flex flex-column container" style="margin-top: 10rem;">
+    <div class="container mt-5">
         <h2>Agendamentos</h2>
+        <div class="table-responsive">
             <table class="table table-bordered text-center">
                 @if(!empty($agendamentos))
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Profissional</th>
+                        <th class="d-none d-md-table-cell">Profissional</th> <!-- Oculto em telas menores que 'md' -->
                         <th>Serviço</th>
                         <th>Data</th>
-                        <th>Horário de Início</th>
-                        <th>Horário de Término</th>
-                        <th>Valor Total</th>
-                        <th>Forma de Pagamento</th>
+                        <th class="d-none d-sm-table-cell">Horário de Início</th> <!-- Oculto em telas menores que 'sm' -->
+                        <th class="d-none d-sm-table-cell">Horário de Término</th> <!-- Oculto em telas menores que 'sm' -->
+                        <th class="d-none d-lg-table-cell">Valor Total</th> <!-- Oculto em telas menores que 'lg' -->
+                        <th class="d-none d-lg-table-cell">Forma de Pagamento</th> <!-- Oculto em telas menores que 'lg' -->
                         <th>Status</th>
                     </tr>
                 </thead>
@@ -25,23 +26,24 @@
                     @foreach ($agendamentos as $agendamento)
                         <tr>
                             <td>{{ $agendamento->id_agendamento }}</td>
-                            <td>{{ $agendamento->profissional }}</td>
+                            <td class="d-none d-md-table-cell">{{ $agendamento->profissional }}</td>
                             <td>{{ $agendamento->servico }}</td>
                             <td>{{ $agendamento->data_realizacao }}</td>
-                            <td>{{ $agendamento->horario_inicio }}</td>
-                            <td>{{ $agendamento->horario_termino }}</td>
-                            <td>{{ $agendamento->valor_total }}</td>
-                            <td>{{ $agendamento->forma_pagamento }}</td>
+                            <td class="d-none d-sm-table-cell">{{ $agendamento->horario_inicio }}</td>
+                            <td class="d-none d-sm-table-cell">{{ $agendamento->horario_termino }}</td>
+                            <td class="d-none d-lg-table-cell">{{ $agendamento->valor_total }}</td>
+                            <td class="d-none d-lg-table-cell">{{ $agendamento->forma_pagamento }}</td>
                             <td>{{ $agendamento->status }}</td>
                         </tr>
                     @endforeach   
                 @else
                     <tr>
-                        <td colspan="4" class="text-center">Nenhum agendamento realizado.</td>
+                        <td colspan="9" class="text-center">Nenhum agendamento realizado.</td>
                     </tr>
                 @endif 
                 </tbody>
             </table>
+        </div>
     </div>
 </section>
 @endsection
