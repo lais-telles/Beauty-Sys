404ec1f54331c79e11b789c7f7a0091ab6869bbb
lais <laismtelles@gmail.com>
2024-10-25 16:44:18 -0300

    Ajuste do idioma das mensagens de erro

---
+++ beautysys/resources/lang/pt-BR/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'min' => [
+        'string' => 'O campo deve ter no mínimo :min caracteres.',
+    ],
+    'max' =>[
+        'string' => 'O campo deve ter no máximo :max caracteres.'
+    ],
+    'unique' => [
+        'string' => 'Email já cadastrado.'
+    ]
+];
