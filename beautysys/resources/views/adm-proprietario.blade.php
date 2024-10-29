16465740722276347490a580e4a354eb3feeed9e
lais <laismtelles@gmail.com>
2024-10-29 10:04:13 -0300

    Inclusão de rota para página de agendamentos

--- beautysys/resources/views/adm-proprietario.blade.php
+++ beautysys/resources/views/adm-proprietario.blade.php
@@ -16,12 +16,14 @@
             <div class="col-md-9">
                 <div class="row justify-content-center">
                     <div class="col-md-4 mb-3">                        
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Agendamentos</h5>
-                                <p class="card-text">Gerencie os agendamentos realizados no seu estabelecimento.</p>
+                        <a href="{{ route('exibirAgendamentosEstab') }}" class="text-decoration-none">
+                            <div class="card">  
+                                <div class="card-body">
+                                    <h5 class="card-title">Agendamentos</h5>
+                                    <p class="card-text">Gerencie os agendamentos realizados no seu estabelecimento.</p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="col-md-4 mb-3">
                         <a href="{{ route('InfoCadastro') }}" class="text-decoration-none">
