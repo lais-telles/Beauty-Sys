73d6ae3e1f21fe4b5d79a2eb846670b602c06715
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-11-03 18:32:48 -0300

    View que será enviada no email do usuário

---
+++ beautysys/resources/views/reset-password.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Redefinição de Senha</title>
+</head>
+<body>
+    <h1>Redefinição de Senha</h1>
+    <p>Olá!</p>
+    <p>Clique no link abaixo para redefinir sua senha:</p>
+    <a href="{{ $link }}">{{ $link }}</a>
+</body>
+</html>
