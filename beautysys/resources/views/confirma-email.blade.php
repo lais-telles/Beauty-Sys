090ff13574450f6df1870e50ddac91ee3f2f7cc7
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2024-11-05 17:12:27 -0300

    View que será enviada por e-mail para a validação do email

---
+++ beautysys/resources/views/confirma-email.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Confirmação de e-mail</title>
+</head>
+<body>
+    <h1>Confirmação de e-mail</h1>
+    <p>Olá!</p>
+    <p>Clique no link abaixo para confirmar seu e-mail:</p>
+    <a href="{{ $link }}">{{ $link }}</a>
+</body>
+</html>
